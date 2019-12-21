4abea7cbe0709360a1f1ab66df074431a9e33ed7
one_piece_best <45225808+daoanhtu57789@users.noreply.github.com>
2019-12-21 15:53:49 +0700

    Add All

--- testBTL/shape.js
+++ testBTL/shape.js
@@ -6,11 +6,12 @@ var VSHADER_SOURCE =
 
 	uniform mat4 u_ModelMatrix;
 	uniform mat4 u_ViewMatrix;
+	uniform mat4 u_CameraMatrix;
 
 	varying vec2 vTextureCoord;
 
 	void main(void) {
-		gl_Position =u_ViewMatrix * u_ModelMatrix *  vec4(aVertexPosition, 1.0);
+		gl_Position = u_ViewMatrix * u_ModelMatrix *  vec4(aVertexPosition, 1.0);
 		vTextureCoord = aTextureCoord;
 	}
 `;
@@ -36,46 +37,11 @@ var FSHADER_SOURCE =
 var gl;
 var shaderProgram;
 var ANGLE_STEP = 45;
-var g_eyeX = 0.0 ,g_eyeY = 0.0 ,g_eyeZ = 0.5;
+var scale_X = 1.0,scale_Y = 1.0,scale_Z=1.0,lookAt_X = 0.0,lookAt_Y = 0.0,lookAt_Z = 1.0;
+var count = 0;
 function main() {
 	// Retrieve <canvas> element
 	var canvas = document.getElementById('webgl');
-	document.onkeydown = ev => {
-		console.log(ev.keyCode);
-		switch(ev.keyCode){
-			
-			case 37:
-				if(-0.5>g_eyeX && g_eyeX > -1.0){
-					g_eyeX -=0.05;
-					break;
-				}
-				g_eyeX = 0.0;
-				break;
-			case 38:
-				if(0.5<g_eyeY<1.0){
-					g_eyeY +=0.05;
-					break;
-				}
-				g_eyeY = 0.0;
-				break;
-			case 39:
-				if(0.5<g_eyeX<1.0){
-					g_eyeX +=0.05;
-					break;
-				}
-				g_eyeX = 0.0;
-				break;
-			case 40:
-				if(-1 < g_eyeY && g_eyeY< -0.5){
-					g_eyeY -=0.05;
-					break;
-				}
-				g_eyeY = 0.0;
-				break;
-			default:
-				break;
-		}
-	}
 	// Get the rendering context for WebGL
 	gl = getWebGLContext(canvas);
 	gl.viewportWidth = canvas.width;
@@ -83,6 +49,59 @@ function main() {
 	if (!gl) {
 	  console.log('Failed to get the rendering context for WebGL');
 	  return;
+	}
+
+	document.onkeydown = ev => {
+		switch (ev.keyCode) 
+		{ 
+			case 37: //mui ten trai
+				count = (count+1)%3;
+				console.log(count);
+				if(count == 0){
+					lookAt_X = 0.0;
+					lookAt_Y = 0.0;
+					lookAt_Z = 1.0;
+				}else if(count == 1){
+					lookAt_X = 0.0;
+					lookAt_Y = 1.0;
+					lookAt_Z = 0.0;
+				}else if(count == 2) {
+					lookAt_X = 1.0;
+					lookAt_Y = 0.0;
+					lookAt_Z = 0.0;
+				}
+				break; 
+			case 38: //mui ten tren
+				scale_X += 0.3;
+				scale_Y += 0.3;
+				scale_Z += 0.3; 
+				break; 
+			case 39: //mui ten phai
+				count = (count - 1.0)%3;
+				//count = Math.abs(count);
+				console.log(count);
+				if(count == -0){
+					lookAt_X = 0.0;
+					lookAt_Y = 0.0;
+					lookAt_Z = 1.0;
+				}else if(count == -1){
+					lookAt_X = 0.0;
+					lookAt_Y = 1.0;
+					lookAt_Z = 0.0;
+				}else if(count == -2){
+					lookAt_X = 1.0;
+					lookAt_Y = 0.0;
+					lookAt_Z = 0.0;
+				}
+				break; 
+			case 40: //mui ten duoi
+				scale_X -= 0.3;
+				scale_Y -= 0.3;
+				scale_Z -= 0.3; 
+				break; 
+			default: 
+				break; 
+		} 
 	}
   
 	//tạo chương trình
@@ -118,7 +137,7 @@ function handleLoadedTexture(texture) {
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 	gl.generateMipmap(gl.TEXTURE_2D);
 
 	gl.bindTexture(gl.TEXTURE_2D, null);
@@ -127,6 +146,7 @@ function handleLoadedTexture(texture) {
 
 var earthColorMapTexture;
 var moonColorMapTexture;
+var spaceColorMapTexture
 function initTextures() {
 	earthColorMapTexture = gl.createTexture();
 	earthColorMapTexture.image = new Image();
@@ -141,7 +161,18 @@ function initTextures() {
             handleLoadedTexture(moonColorMapTexture)
         }
 
-    moonColorMapTexture.image.src = "moon.gif";
+	moonColorMapTexture.image.src = "moon.gif";
+	
+
+
+	spaceColorMapTexture = gl.createTexture();
+    spaceColorMapTexture.image = new Image();
+    spaceColorMapTexture.image.onload = function () {
+            handleLoadedTexture(spaceColorMapTexture)
+        }
+
+	spaceColorMapTexture.image.src = "sao.jpg";
+	console.log(spaceColorMapTexture.image);
 }
 
 var viewMatrix = new Matrix4();
@@ -150,8 +181,9 @@ var modelMatrix= new Matrix4();
 
 // Ma trận biến đổi tọa độ
 function setMatrixUniforms() {
-	viewMatrix.setLookAt(g_eyeX,g_eyeY,g_eyeZ,0,0,0,0,1,0);
-	
+	viewMatrix.setPerspective(30, gl.viewportWidth / gl.viewportHeight, 1.0, 100.0);
+	viewMatrix.lookAt( 1.0 , 0.0, 5.0, 0.0, 0.0,0.0, lookAt_X, lookAt_Y, lookAt_Z);
+	console.log(lookAt_X, lookAt_Y, lookAt_Z);
 	gl.uniformMatrix4fv(shaderProgram.u_ModelMatrix,false,modelMatrix.elements);
 	gl.uniformMatrix4fv(shaderProgram.u_ViewMatrix,false,viewMatrix.elements);
 
@@ -297,15 +329,17 @@ function initBuffers2() {
 	sphereVertexIndexBuffer2.itemSize = 1;
 	sphereVertexIndexBuffer2.numItems = indexData.length;
 }
-
+// var vertexTexCoordBuffer;
 
 
 
 var currentAngle = 0.0;
+var moonaAngle=0.0;
 
 function drawScene1() {
-	
+
 	modelMatrix.setRotate(currentAngle,0,0,1);
+	modelMatrix.scale(scale_X,scale_Y,scale_Z);
 	gl.activeTexture(gl.TEXTURE0);
 	gl.bindTexture(gl.TEXTURE_2D, earthColorMapTexture);
 	gl.uniform1i(shaderProgram.colorMapSamplerUniform, 0);
@@ -323,8 +357,10 @@ function drawScene1() {
 }
 
 function drawScene2() {
-	modelMatrix.rotate(currentAngle,0,0,1);
-	modelMatrix.translate(0.8,0,0);
+
+	modelMatrix.setRotate(currentAngle, 0, 0, 1);
+	modelMatrix.scale(scale_X,scale_Y,scale_Z);
+	modelMatrix.translate(1.5, 0.0, 0.0)
 	gl.activeTexture(gl.TEXTURE1);
 	gl.bindTexture(gl.TEXTURE_2D, moonColorMapTexture);
 	gl.uniform1i(shaderProgram.colorMapSamplerUniform, 1);
@@ -341,6 +377,43 @@ function drawScene2() {
 	gl.drawElements(gl.TRIANGLES, sphereVertexIndexBuffer2.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
+function drawScene3() {
+	modelMatrix.setTranslate(0,0,-4);
+
+	var verticesTexCoords = new Float32Array([
+		// Vertex coordinates, texture coordinate
+		-5,  5,   0.0, 1.0,
+		-5, -5,   0.0, 0.0,
+		 5,  5,   1.0, 1.0,
+		 5, -5,   1.0, 0.0,
+	]);
+	// Create the buffer object
+	var vertexTexCoordBuffer = gl.createBuffer();
+
+	// Bind the buffer object to target
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
+
+	var FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
+	//Get the storage location of a_Position, assign and enable buffer
+	
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 2, gl.FLOAT, false, FSIZE * 4, 0);
+	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);  // Enable the assignment of the buffer object
+
+	// Assign the buffer object to a_TexCoord variable
+	gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+	gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);  // Enable the assignment of the buffer object
+	
+	gl.activeTexture(gl.TEXTURE2);
+	gl.bindTexture(gl.TEXTURE_2D, spaceColorMapTexture);
+	gl.uniform1i(shaderProgram.colorMapSamplerUniform, 2);
+
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+
+
 
 var lastTime = Date.now();
 
@@ -351,6 +424,13 @@ function animate(angle) {
 	var newAngle = angle +(ANGLE_STEP * elapsed)/1000.0;
 	return newAngle%360;
 }
+function animate2(angle1) {
+	var timeNow = Date.now();
+	var elapsed = timeNow - lastTime;
+	lastTime = timeNow;
+	var newAngle1 = angle1 +(ANGLE_STEP1 * elapsed)/1000.0;
+	return newAngle1%360;
+}
 
 function tick() {
 	window.requestAnimationFrame(tick);
@@ -358,7 +438,7 @@ function tick() {
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	drawScene1();
 	drawScene2();
+	drawScene3();
 	currentAngle = animate(currentAngle);
+	
 }
-
-
